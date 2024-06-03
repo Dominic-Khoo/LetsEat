7a0cbff38b20d72941396325e82e4465106739c2
Dominic-Khoo <dominickhooyx@gmail.com>
2024-06-03 12:12:08 +0800

    Milestone 1

--- LetsEat/firebaseConfig.js
+++ LetsEat/firebaseConfig.js
@@ -1,23 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import dotenv from 'dotenv';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+dotenv.config();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC9bE8uBIL_4h9dWAcLgKrG2mOtGxYViIc",
-  authDomain: "letseat-5ecc4.firebaseapp.com",
-  databaseURL: "https://letseat-5ecc4-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "letseat-5ecc4",
-  storageBucket: "letseat-5ecc4.appspot.com",
-  messagingSenderId: "617476778354",
-  appId: "1:617476778354:web:618f39b3e3c09e5bf655eb",
-  measurementId: "G-63D59QHMKY"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
